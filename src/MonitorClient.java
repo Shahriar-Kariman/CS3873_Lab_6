32a117c4c031bf84d81e541a38b797ff60d2dafb
Shahriar Kariman <shahriar.kariman@gmail.com>
2025-03-29 19:51:04 -0300

    Started writing the client.

--- src/MonitorClient.java
+++ src/MonitorClient.java
@@ -4,18 +4,33 @@ import java.net.*;
 public class MonitorClient {
   public static void main(String[] args) throws Exception {
     
+    InetAddress IPAddress = InetAddress.getByName(args[0]);
+    int PortNumber = Integer.parseInt(args[1]);
+
     DatagramSocket clientSocket = new DatagramSocket();
     InetAddress serverIP = InetAddress.getByName("Hostname");
 
-    byte[] message = "Message".getBytes();
-    DatagramPacket send_packet = new DatagramPacket(message, message.length);
+    for(int i=0; i<40; i++){
+      long rtt = -1;
 
-    clientSocket.send(send_packet);
-    
-    byte[] reply = new byte[1024];
-    DatagramPacket replyPacket = new DatagramPacket(reply, reply.length);
-
-    clientSocket.receive(replyPacket);
+      byte[] message = ("Hello "+ i).getBytes();
+      DatagramPacket send_packet = new DatagramPacket(message, message.length, IPAddress, PortNumber);
+  
+      long startTime = System.currentTimeMillis();
+      clientSocket.send(send_packet);
+      
+      byte[] reply = new byte[1024];
+      DatagramPacket replyPacket = new DatagramPacket(reply, reply.length);
+      
+      clientSocket.setSoTimeout(1000);
+      try {
+        clientSocket.receive(replyPacket);
+        long endTime = System.currentTimeMillis();
+        rtt = endTime - startTime;
+      } catch (Exception e) {
+        // There was no message
+      }
+    }
 
     clientSocket.close();
   }
